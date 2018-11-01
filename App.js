a39bc8cc4d37563b4c464c2249d7d0ebcbc8bae3
knguyen1 <knguyen1@paypal.com>
2018-10-31 23:45:48 -0700

    Add react-native-button & react-navigation

--- App.js
+++ App.js
@@ -4,18 +4,51 @@
  */
 
 import React from "react";
-import { StyleSheet, Button, Text, StatusBar, View } from "react-native";
+import { StyleSheet, Text, StatusBar, View } from "react-native";
 import { Constants } from "expo";
+import { createStackNavigator } from "react-navigation";
+import Button from "react-native-button";
 
-class App extends React.Component {
-  handlePress = e => {
-    console.log("pressed");
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Caturday"
   };
 
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
         <View style={styles.statusBar} />
+        <View style={styles.navBar}>
+          <Text style={styles.navBarText}>Caturday</Text>
+        </View>
+        <View style={styles.mainContent}>
+          <View style={styles.buttonView}>
+            <Button
+              onPress={() => navigate("AddList", { data: "Cats" })}
+              style={{
+                color: "#000",
+                backgroundColor: "#fff",
+                fontSize: 25,
+                borderWidth: 1,
+                textAlign: "left",
+                padding: 20
+              }}
+            >
+              + New List
+            </Button>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+class AddListScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
       </View>
     );
   }
@@ -32,7 +65,27 @@ const styles = StyleSheet.create({
   },
   statusBar: {
     height: Constants.statusBarHeight
+  },
+  navBar: {
+    height: 51,
+    backgroundColor: COLOR_MAGENTA,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  navBarText: {
+    color: "#fff",
+    fontSize: 20
+  },
+
+  mainContent: {
+    padding: 20,
+    backgroundColor: "#fff"
   }
 });
 
+// Define routes
+const App = createStackNavigator({
+  Home: { screen: HomeScreen },
+  AddList: { screen: AddListScreen }
+});
 export default App;
