16287a51378e7869a6f0c8aa4a41312ae27f5e4a
knguyen1 <knguyen1@paypal.com>
2018-11-04 21:51:13 -0800

    Add random 'tab' color to the ListCard component

--- components/ListCard.js
+++ components/ListCard.js
@@ -6,7 +6,7 @@ export default class ListCard extends Component {
   render() {
     const { handlePress } = this.props;
     return (
-      <View>
+      <View style={[styles.view, { borderLeftColor: generateRandomColor() }]}>
         <Button onPress={handlePress} style={styles.listCard}>
           {this.props.children}
         </Button>
@@ -15,23 +15,31 @@ export default class ListCard extends Component {
   }
 }
 
+const generateRandomColor = () => {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 const styles = StyleSheet.create({
-  listCard: {
-    color: "#000",
-    backgroundColor: "#fff",
-    fontSize: 25,
-    // borderWidth: 1,
-    // borderColor: "#aaa",
+  view: {
     borderLeftWidth: 5,
-    borderLeftColor: "green",
-    textAlign: "left",
-    padding: 20,
     marginBottom: 10,
+    backgroundColor: "#fff",
     shadowColor: "#111",
     shadowOffset: {
       height: 3
     },
     shadowOpacity: 0.25,
     shadowRadius: 3
+  },
+  listCard: {
+    color: "#000",
+    textAlign: "left",
+    fontSize: 20,
+    padding: 20
   }
 });
