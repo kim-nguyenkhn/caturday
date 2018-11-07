98ed090905747ebb7318c4df5c80bdc7c4c903aa
knguyen1 <knguyen1@paypal.com>
2018-11-07 10:32:46 -0800

    fix: Format typeDefs & resolvers properly as per graphql-yoga

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const { GraphQLServer } = require("graphql-yoga");
-const { buildSchema } = require("graphql");
 
-// GraphQL schema - could later be moved out to its own .graphql file
-const schema = buildSchema(`
+// TEMP: Simulating a DB
+let LISTS_COLLECTION = [
+  {
+    id: 1,
+    title: "Kitties"
+  },
+  {
+    id: 2,
+    title: "Groceries"
+  }
+];
+
+// GraphQL schema definition - could later be moved out to its own schema.graphql file
+const typeDefs = `
   type Query {
-    list(id: Int!): List
-    lists: [List]
+    list(id: Int!): List!
+    lists: [List]!
   }
   type Mutation {
     createList(title: String!): List!
@@ -22,51 +33,48 @@ const schema = buildSchema(`
     title: String
     isChecked: Boolean
   }
-`);
+`;
 
-// TEMP: Simulating a DB
-let LISTS_COLLECTION = [
-  {
-    id: 1,
-    title: "Kitties"
-  },
-  {
-    id: 2,
-    title: "Groceries"
-  }
-];
-
-// Root resolver
+// Root resolver - functions have the following arguments: (parent, args, ctx, info)
 const resolvers = {
-  list: args => {
-    const id = args.id;
-    return LISTS_COLLECTION.filter(list => {
-      return list.id === id;
-    })[0];
+  Query: {
+    list: args => {
+      const id = args.id;
+      return LISTS_COLLECTION.filter(list => {
+        return list.id === id;
+      })[0];
+    },
+    lists: () => {
+      return LISTS_COLLECTION;
+    }
   },
-  lists: () => LISTS_COLLECTION,
-  createList: ({ title }) => {
-    const newList = {
-      id: LISTS_COLLECTION.length + 1,
-      title
-    };
-    LISTS_COLLECTION.push(newList);
-    return newList;
-  },
-  updateList: ({ id, message }) => {},
-  cleanDB: () => {
-    LISTS_COLLECTION = [];
-    return true;
+  Mutation: {
+    createList: (parent, { title }) => {
+      const newList = {
+        id: LISTS_COLLECTION.length + 1,
+        title
+      };
+      LISTS_COLLECTION.push(newList);
+      return newList;
+    },
+    updateList: ({ id, message }) => {},
+    cleanDB: () => {
+      LISTS_COLLECTION = [];
+      return true;
+    }
   }
 };
 
+// GraphQL Yoga API: https://www.prisma.io/docs/1.8/graphql-ecosystem/graphql-yoga/overview-chaha122ho#api
 const server = new GraphQLServer({
-  schema,
-  resolvers
-  // resolverValidationOptions: {
-  //   requireResolversForResolveType: false
-  // }
+  typeDefs,
+  resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  }
 });
+
+// Define some options for the server
 const graphQLYogaOptions = {
   port: 4000,
   endpoint: "/graphql",
@@ -77,6 +85,8 @@ const graphQLYogaOptions = {
     origin: "http://localhost:4000"
   }
 };
+
+// Run the server
 server.start(graphQLYogaOptions, () => {
   console.log(
     `Server is now running on port http://localhost:${graphQLYogaOptions.port}`
